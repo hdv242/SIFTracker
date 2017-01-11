8f03a84d05d4b295cf0f894200499d5d7192bb21
Hiep Vu <h.vu1995@yahoo.com>
2017-01-10 21:20:23 -0600

    implemented i/o for song_stats

--- src/__main__.py
+++ src/__main__.py
@@ -10,28 +10,41 @@ from tkinter import Label
 from tkinter import PhotoImage
 from tkinter import Tk
 from tkinter import messagebox
-import matches
+from matches import Song
 
-entries = []
-songs = {'Sunny Day Song': 0}
+songName_i = 0  # index of song name in entries
+score_i = 1     # index of score in entries
+combo_i = 2     # index of combo in entries
+gui = Tk()      # creates the gui
+entries = []    # songName, score, combo, and rank
+songsList = {}  # songs will be eager cached as name : object
 
+
+## writes submitted information into file
+def writeToFile():
+    with open("song_stats.txt", 'w') as file :
+        for key in songsList.keys() :
+            songsList[key].write(file)
+            file.write("\n")
+    
+    #with open("score_match_stats.txt", 'a') as file :
+    #    for e in entries :
+    #        file.write(e.get() + " ")
+    #    file.write("\n")
+    file.close()
+
+
+## exits app
+def exitApp() :
+    writeToFile()
+    gui.quit()
+    
+    
 ## clears out entry boxes
 def clearEntries() :
     for e in entries :
         e.delete(0, 'end')
 
-## writes submitted information into file
-def writeToFile():
-    with open("score_match_stats.txt", 'a') as file :
-        for e in entries :
-            file.write(e.get() + " ")
-        file.write("\n")
-    file.close()
-    clearEntries()
-
-## verifies the inputed song is a real song
-def verifySong() :
-    return
 
 ## facilitates input of information
 def inputAnswer() :
@@ -41,20 +54,46 @@ def inputAnswer() :
         if (e.get() == "") :
             emptyFields += 1
     if (emptyFields > 0) :
-        messagebox.showinfo("Failed!", "Missing field(s)")
+        messagebox.showerror("Failed!", "Missing field(s)")
         return
-            
-    writeToFile()
+    
+    # check for incorrect song name
+    name = entries[songName_i].get().lower() + " "
+    if (songsList.get(name) == None) :
+        messagebox.showerror("Failed!", "Cannot find song")
+        return
+    
+    # update song info
+    songsList.get(name).play(entries[score_i].get(), entries[combo_i].get())
+    clearEntries()
     messagebox.showinfo("Success!", "Info submitted")
 
-if __name__ == "__main__"  :
-    gui = Tk()
-    gui.title("SIF Score Match Tracker")
-    gui.geometry("524x333")
-    
+
+## add songs from file
+def addSongs() :
+    with open("song_stats.txt") as file :
+        for line in file :
+            # parse from file, fix name, create new Song and add it to songsList
+            curSong = line.split();
+            curName = curSong[songName_i].split('_')
+            tempName = ""
+            for part in curName :
+                tempName += part + " "
+            tempName = tempName.lower()
+            
+            songsList[tempName] = (Song(curSong[songName_i],
+                                        curSong[1], curSong[2], curSong[3],
+                                        curSong[4], curSong[5], curSong[6],
+                                        curSong[7], curSong[8], curSong[9]))
+    file.close()
+ 
+
+## Score Match interface
+def smgui() : 
     # background image
     bgImage = PhotoImage(file = "const-maki.png")
     backgroundLabel = Label(gui, image = bgImage)
+    backgroundLabel.image = bgImage
     backgroundLabel.place(x = 0, y = 0, relwidth = 1, relheight = 1)
     
     # song name input field
@@ -107,8 +146,14 @@ if __name__ == "__main__"  :
                                   command = clearEntries)
     clearButton.place(x = buttonX + 70, y = buttonY)
     cancelButton = Button(gui, text = "Cancel", cursor = "heart",
-                                  command = gui.quit)
+                                  command = exitApp)
     cancelButton.place(x = buttonX + 130, y = buttonY)
-    
+
+
+if __name__ == "__main__"  :
+    gui.title("SIF Score Match Tracker")
+    gui.geometry("524x333")
+    addSongs()
+    smgui()
     gui.mainloop()
     
--- src/matches.py
+++ src/matches.py
@@ -14,18 +14,58 @@ class Match :
         
 class Song :
     
-    def __init__(self, songName, attribute, maxCombo) :
-        self.songName = songName
-        self.attribute = attribute
-        self.maxCombo = maxCombo
-        self.plays = 0
-        self.fc = 0
+    def __init__(self, songName, attribute, diff, fcCombo, maxScore, maxCombo,
+                 avgScore, avgCombo, plays, fc) :
         
-    def play(self) :
+        self.songName  = songName
+        self.attribute = attribute
+        self.diff      = diff
+        self.fcCombo   = int(fcCombo)
+        self.maxScore  = int(maxScore)
+        self.maxCombo  = int(maxCombo)
+        self.avgScore  = float(avgScore)
+        self.avgCombo  = float(avgCombo)
+        self.plays     = int(plays)
+        self.fc        = int(fc)
+        
+        
+    def play(self, score, combo) :
         self.plays += 1
+        score = int(score)
+        combo = int(combo)
+        
+        # update scores and combos
+        if (self.plays != 0) :
+            self.avgScore = (self.avgScore * (self.plays - 1) + score) / self.plays
+            self.avgCombo = (self.avgCombo * (self.plays - 1) + combo) / self.plays
+        else :
+            self.avgScore = score
+            self.avgCombo = combo
+        
+        if (self.maxScore < score) :
+            self.maxScore = score;
+        if (self.maxCombo < combo) :
+            self.maxCombo = combo
+        if (self.fcCombo == combo) :
+            self.fc += 1
+        
     
-    def fc(self) :
-        self.fc += 1
+    def name (self) :
+        return self.songName
+    
         
     def fcRate(self) :
         return self.fc / self.plays
+    
+    
+    def write(self, file) :
+        file.write(self.songName + " " + 
+                   self.attribute + " " +
+                   self.diff + " " +
+                   str(self.fcCombo) + " " +
+                   str(self.maxScore) + " " +
+                   str(self.maxCombo) + " " +
+                   str(self.avgScore) + " " +
+                   str(self.avgCombo) + " " +
+                   str(self.plays) + " " +
+                   str(self.fc))
